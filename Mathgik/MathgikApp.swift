35801c716717e61ecd3d98764b6f1426cdb6cb3b
Louis Mille <mille.louis1@gmail.com>
2023-07-16 10:48:27 +0200

    Initial Commit

---
+++ Mathgik/MathgikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathgikApp.swift
+//  Mathgik
+//
+//  Created by Louis Mille on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MathgikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
